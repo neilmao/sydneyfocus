357dd25d75c7dd9cffaceeb05b88a36967d70fc5
neilmao <neil.xy.mao@gmail.com>
2015-04-23 00:55:05 +1000

    Change “News” class

---
+++ sydneyfocus-data/src/main/java/com/sydneyfocus/NewsCategory.java
@@ -0,0 +1,15 @@
+package com.sydneyfocus;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: neilmao
+ * Date: 23/04/15
+ */
+public enum NewsCategory {
+
+    Current,
+    Sport,
+    Entertainment,
+    Funny;
+
+}
